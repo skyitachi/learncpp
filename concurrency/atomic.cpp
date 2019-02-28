850b5d940751822ae38c911bf61482e955aa11fd
skyitachi <skbysp@gmail.com>
2019-02-28 21:56:40 +0800

    store code

--- concurrency/atomic.cpp
+++ concurrency/atomic.cpp
@@ -36,6 +36,33 @@ void consumer() {
   assert(data == 10); // ensured
 }
 
+// 5.9
+std::atomic<int> gData[5];
+std::atomic<bool> sync1(false), sync2(false);
+
+void thread_1() {
+  gData[0].store(1, std::memory_order_relaxed);
+  gData[1].store(2, std::memory_order_relaxed);
+  gData[2].store(3, std::memory_order_relaxed);
+  gData[3].store(4, std::memory_order_relaxed);
+  gData[4].store(5, std::memory_order_relaxed);
+  sync1.store(true, std::memory_order_release);
+}
+
+void thread_2() {
+  while(!sync1.load(std::memory_order_acquire));
+  sync2.store(true, std::memory_order_release);
+}
+
+void thread_3() {
+  while(!sync2.load(std::memory_order_acquire));
+  assert(gData[0].load(std::memory_order_relaxed) == 1);
+  assert(gData[1].load(std::memory_order_relaxed) == 2);
+  assert(gData[2].load(std::memory_order_relaxed) == 3);
+  assert(gData[3].load(std::memory_order_relaxed) == 4);
+  assert(gData[4].load(std::memory_order_relaxed) == 5);
+}
+
 int main() {
   x = false;
   y = false;
