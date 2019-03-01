3027669f08ee2f190e335a034bbe255d2008ca4a
skyitachi <skbysp@gmail.com>
2019-03-01 11:59:50 +0800

    store code

--- concurrency/atomic.cpp
+++ concurrency/atomic.cpp
@@ -40,6 +40,8 @@ void consumer() {
 std::atomic<int> gData[5];
 std::atomic<bool> sync1(false), sync2(false);
 
+std::atomic<int> syncCAS(0);
+
 void thread_1() {
   gData[0].store(1, std::memory_order_relaxed);
   gData[1].store(2, std::memory_order_relaxed);
@@ -47,15 +49,26 @@ void thread_1() {
   gData[3].store(4, std::memory_order_relaxed);
   gData[4].store(5, std::memory_order_relaxed);
   sync1.store(true, std::memory_order_release);
+  
+  // cas style
+//  syncCAS.store(1, std::memory_order_release);
 }
 
 void thread_2() {
   while(!sync1.load(std::memory_order_acquire));
   sync2.store(true, std::memory_order_release);
+  // cas style
+//  int expected = 1;
+//  while(!syncCAS.compare_exchange_strong(expected, 2, std::memory_order_acq_rel)) {
+//    expected = 1;
+//  }
 }
 
 void thread_3() {
   while(!sync2.load(std::memory_order_acquire));
+  
+  // cas style
+//  while(!syncCAS.load(std::memory_order_acquire) < 2);
   assert(gData[0].load(std::memory_order_relaxed) == 1);
   assert(gData[1].load(std::memory_order_relaxed) == 2);
   assert(gData[2].load(std::memory_order_relaxed) == 3);
