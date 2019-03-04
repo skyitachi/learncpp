e21b6332bca5dada8e284861a150d4bf7e86e709
skyitachi <skbysp@gmail.com>
2019-03-04 17:27:52 +0800

    feat: add atomic_thread_fence

--- concurrency/atomic.cpp
+++ concurrency/atomic.cpp
@@ -16,11 +16,14 @@ int data = 0;
 
 void write_x_then_y() {
   x.store(true, std::memory_order_relaxed);
+  // barrier
+  std::atomic_thread_fence(std::memory_order_release);
   y.store(true, std::memory_order_relaxed);
 }
 
 void read_y_then_x() {
   while(!y.load(std::memory_order_relaxed));
+  std::atomic_thread_fence(std::memory_order_acquire);
   if(x.load(std::memory_order_relaxed))
     ++z;
 }
