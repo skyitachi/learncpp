e1a0cc24a1f2819ba2632c4c62ddb216d2e7ecb2
skyitachi <skbysp@gmail.com>
2022-04-04 22:33:26 +0800

    sync code

--- faiss_demo/main.cpp
+++ faiss_demo/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <faiss/IndexFlat.h>
 #include <string>
 #include <grpc/grpc.h>
 
@@ -11,6 +10,9 @@
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 
+#include <faiss/IndexFlat.h>
+#include <faiss/IndexIVFFlat.h>
+
 using idx_t = faiss::Index::idx_t;
 
 using grpc::Status;
@@ -19,8 +21,8 @@ using grpc::ServerContext;
 using grpc::ServerBuilder;
 class FaissIndexImpl final: public FaissIndex::Service {
 public:
-	explicit FaissIndexImpl(faiss::IndexFlatL2 *index) {
-		index_flat_l2_ = index;
+	explicit FaissIndexImpl(faiss::IndexIVFFlat *index) {
+		index_ivf_flat_ = index;
 		
 	}
 
@@ -34,10 +36,10 @@ public:
 	} 
 
 private:
-	faiss::IndexFlatL2 *index_flat_l2_;
+	faiss::IndexIVFFlat *index_ivf_flat_;
 };
 
-void start_grpc_server(faiss::IndexFlatL2 *index) {
+void start_grpc_server(faiss::IndexIVFFlat *index) {
 	std::string server_address("0.0.0.0:10001");
 	FaissIndexImpl service(index);
 	
@@ -57,53 +59,7 @@ void start_grpc_server(faiss::IndexFlatL2 *index) {
 	
 }
 int main() {
-
-	int d = 64;
-	int nb = 100;
-	int nq = 10;
-	float *xb = new float[d * nb];
-	float *xq = new float[d * nq];
-	for (int i = 0; i < nb; i++) {
-		for (int j = 0; j < d; j++) xb[d * i + j] = drand48();
-		xb[d * i] += i / 1000.;
-	}
-
-	for (int i = 0; i < nq; i++) {
-		for (int j = 0; j < d; j++) xq[d * i + j] = drand48();
-
-		xq[d *i] += i / 1000.;
-	}
-
-
-	faiss::IndexFlatL2 index(d);
-	printf("is_trained = %s\n", index.is_trained ? "true" : "false");
-	index.add(nb, xb);
-
-	int k = 4;
-	{
-		idx_t *I = new idx_t[k * 5];
-		float* D = new float[k * 5];
-
-		index.search(5, xb, k, D, I);
-
-		printf("I=\n");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < k; j++)
-                printf("%5zd ", I[i * k + j]);
-            printf("\n");
-        }
-
-        printf("D=\n");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < k; j++)
-                printf("%7g ", D[i * k + j]);
-            printf("\n");
-        }
-
-        delete[] I;
-        delete[] D;
-	}
+	IndexIVFFlat *index = dynaimic_cast<IndexIVFFlat*>(faiss::read_index("saved_item.idx"));
 	start_grpc_server(&index);
 	return 0;
-
 }
