f6351adf853af84ef818394fc7318e658bdefd11
skyitachi <skbysp@gmail.com>
2022-04-04 23:06:04 +0800

    sync data

--- faiss_demo/main.cpp
+++ faiss_demo/main.cpp
@@ -10,8 +10,11 @@
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 
+#include <faiss/index_io.h>
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFFlat.h>
+
+#include <mutex>
 
 using idx_t = faiss::Index::idx_t;
 
@@ -19,6 +22,8 @@ using grpc::Status;
 using grpc::Server;
 using grpc::ServerContext;
 using grpc::ServerBuilder;
+
+const int DIM = 128;
 class FaissIndexImpl final: public FaissIndex::Service {
 public:
 	explicit FaissIndexImpl(faiss::IndexIVFFlat *index) {
@@ -27,16 +32,42 @@ public:
 	}
 
 	Status Search(ServerContext* context, const SearchRequest* req, SearchResponse* res) override {
-		std::cout << "k is: " << req->k() << std::endl;
-		res->set_msg("hello world");
-		res->set_code(1);
-		res->add_distance(1.0);
-		res->add_labels(1);
+		std::lock_guard<std::mutex> lock_guard(index_mu_);
+		std::cout << "k is: " << req->k() <<" vector_size:" << req->vec_size()<< std::endl;
+		int k = req->k();
+		if (k == 0) {
+			res->set_msg("unexpect k");
+			res->set_code(1);
+			return Status::OK;
+		}
+		if (req->vec_size() != DIM) {
+			res->set_msg("unexpect vector length");
+			res->set_code(1);
+			return Status::OK;
+		}
+
+		float *q = new float[DIM];
+		for (int i = 0; i < DIM; i++) {
+			q[i] = req->vec(i);
+		}
+	
+
+		float* dis = new float[k];
+		idx_t* labels = new idx_t[k];
+		if (index_ivf_flat_ != nullptr) {
+			index_ivf_flat_->search(1, q, k, dis, labels);
+		}
+		res->set_msg("ok");
+		for (int i = 0; i < k; i++) {
+			res->add_distance(dis[i]);
+			res->add_labels(labels[i]);
+		}
 		return Status::OK;
 	} 
 
 private:
 	faiss::IndexIVFFlat *index_ivf_flat_;
+	std::mutex index_mu_;
 };
 
 void start_grpc_server(faiss::IndexIVFFlat *index) {
@@ -59,7 +90,8 @@ void start_grpc_server(faiss::IndexIVFFlat *index) {
 	
 }
 int main() {
-	IndexIVFFlat *index = dynaimic_cast<IndexIVFFlat*>(faiss::read_index("saved_item.idx"));
-	start_grpc_server(&index);
+	faiss::IndexIVFFlat *index = dynamic_cast<faiss::IndexIVFFlat*>(
+		faiss::read_index("saved_item.idx"));
+	start_grpc_server(index);
 	return 0;
 }
