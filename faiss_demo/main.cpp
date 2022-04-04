bee31987909a060e710364de91e9ea91c5003363
skyitachi <skbysp@gmail.com>
2022-04-04 22:14:53 +0800

    sync code

--- faiss_demo/main.cpp
+++ faiss_demo/main.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
 #include <faiss/IndexFlat.h>
+#include <string>
+#include <grpc/grpc.h>
+
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include "common.grpc.pb.h"
+#include "common.pb.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
 
 using idx_t = faiss::Index::idx_t;
 
+using grpc::Status;
+using grpc::Server;
+using grpc::ServerContext;
+using grpc::ServerBuilder;
+class FaissIndexImpl final: public FaissIndex::Service {
+public:
+	explicit FaissIndexImpl(faiss::IndexFlatL2 *index) {
+		index_flat_l2_ = index;
+		
+	}
+
+	Status Search(ServerContext* context, const SearchRequest* req, SearchResponse* res) override {
+		std::cout << "k is: " << req->k() << std::endl;
+		res->set_msg("hello world");
+		res->set_code(1);
+		res->add_distance(1.0);
+		res->add_labels(1);
+		return Status::OK;
+	} 
+
+private:
+	faiss::IndexFlatL2 *index_flat_l2_;
+};
+
+void start_grpc_server(faiss::IndexFlatL2 *index) {
+	std::string server_address("0.0.0.0:10001");
+	FaissIndexImpl service(index);
+	
+	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
+	ServerBuilder builder;
+
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+	builder.RegisterService(&service);
+
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+
+	std::cout << "Server Listening on" << server_address << std::endl;
+	server->Wait();
+
+	
+}
 int main() {
 
 	int d = 64;
@@ -50,6 +103,7 @@ int main() {
         delete[] I;
         delete[] D;
 	}
+	start_grpc_server(&index);
 	return 0;
 
 }
