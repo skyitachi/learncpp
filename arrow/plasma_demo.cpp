9e7b12da743b112370e68b4283a917f68bb0b1b8
Shiping Yao <shiping.yao@shopee.com>
2022-11-13 18:31:54 +0800

    make plasma demo works

--- arrow/plasma_demo.cpp
+++ arrow/plasma_demo.cpp
@@ -6,30 +6,66 @@
 #include <plasma/client.h>
 #include <thread>
 #include <chrono>
+#include <arrow/api.h>
+#include <arrow/result.h>
+#include <arrow/util/logging.h>
+#include <plasma/test_util.h>
 
 using namespace plasma;
 
-int main() {
-  PlasmaClient client;
-  client.Connect("/tmp/plasma");
-  ObjectID object_id = ObjectID::from_binary("00000000000000000010");
-  int64_t data_size = 1000;
+void get_object_demo(PlasmaClient& client, ObjectID& oid) {
+  ObjectBuffer object_buffer;
+  std::cout << "get_demo oid: " << oid.hex() << std::endl;
+  ARROW_CHECK_OK(client.Get(&oid, 1, -1, &object_buffer));
+  auto buffer = object_buffer.data;
+  const uint8_t* data = buffer->data();
+  int64_t data_size = buffer->size();
+  std::cout << "data_size: " << data_size << std::endl;
+
+  for (int64_t i = 0; i < data_size; i++) {
+    ARROW_CHECK(data[i] == static_cast<uint8_t>(i % 4));
+  }
+}
+
+void write_demo(PlasmaClient& client, ObjectID& oid) {
+  // NOTE: 这里不能随意调用
+//  ObjectID object_id = random_object_id();
+//  std::string id_string = object_id.binary();
+
+  std::cout << "write_demo oid: " << oid.hex() << std::endl;
+
+  bool has_object;
+  ARROW_CHECK_OK(client.Contains(oid, &has_object));
+  if (has_object) {
+    std::cout << "found object " << oid.hex() << std::endl;
+    return;
+  }
+  int64_t data_size = 100;
   std::shared_ptr<Buffer> data;
-  std::string metadata = "{'author': 'john'}";
-
-//  client.Create(object_id, data_size, (uint8_t*) metadata.data(), metadata.size(), &data);
-  client.Create(object_id, data_size, NULL, 0, &data);
-
+  ARROW_CHECK_OK(client.Create(oid, data_size, NULL, 0, &data));
 
   auto d = data->mutable_data();
-  for (int64_t i = 0; i < data_size; i++) {
+  for (int64_t i =0; i < data_size; i++) {
     d[i] = static_cast<uint8_t>(i % 4);
   }
 
-  client.Seal(object_id);
+  ARROW_CHECK_OK(client.Seal(oid));
+}
 
-  using namespace std::literals::chrono_literals;
-  std::this_thread::sleep_for(10s);
-  // Disconnect the client.
-  client.Disconnect();
+int main() {
+  // NOTE: 和python之间的通信需要使用python client.get_buffers api
+  // plasma_store_server 不停的话，数据会一直在
+  PlasmaClient client;
+  ARROW_CHECK_OK(client.Connect("/tmp/plasma"));
+  ObjectID oid = ObjectID::from_binary("00000000000000000000");
+
+  std::thread w_th([&]{
+    write_demo(client, oid);
+  });
+  std::thread r_th([&]{
+    get_object_demo(client, oid);
+  });
+  w_th.join();
+  r_th.join();
+  ARROW_CHECK_OK(client.Disconnect());
 }
