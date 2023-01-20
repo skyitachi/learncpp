54edd118b2faf79293ade3f8a9e64ca9af5407f6
skyitachi <skbysp@gmail.com>
2023-01-21 00:14:33 +0800

    sync code

--- folly/main.cpp
+++ folly/main.cpp
@@ -3,11 +3,33 @@
 //
 #include <folly/FBString.h>
 #include <iostream>
+#include <string>
 
 using namespace folly;
 
 int main() {
-  auto s1 = folly::fbstring("abc");
-  fbstring s2("abcd");
-  std::cout << s1 << ", " << s2 << std::endl;
+  {
+    auto s1 = folly::fbstring("abc");
+    fbstring s2("abcd");
+    std::cout << s1 << ", " << s2 << std::endl;
+  }
+
+  // verify cow
+  {
+    folly::fbstring bs1;
+    bs1.resize(1000);
+    printf("big fb_string s1 data pointer %p\n", bs1.c_str());
+
+    auto bs2 = bs1;
+    printf("after copy constructor fb_string bs2 data pointer %p\n", bs2.c_str());
+  }
+  {
+    std::string s1;
+    s1.resize(1000);
+
+    printf("big std string s1 data pointer %p\n", s1.c_str());
+
+    auto bs2 = s1;
+    printf("after copy constructor bs2 data pointer %p\n", bs2.c_str());
+  }
 }
