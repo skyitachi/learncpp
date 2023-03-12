767712c5793c740ea004a502b509f0790d1030b3
Shiping Yao <shiping.yao@shopee.com>
2023-03-12 09:40:05 +0800

    abseil demo

--- abseil_tips_demo/main.cpp
+++ abseil_tips_demo/main.cpp
@@ -5,6 +5,8 @@
 #include <string_view>
 #include <absl/strings/str_cat.h>
 #include <absl/strings/str_join.h>
+#include <absl/strings/str_replace.h>
+#include <absl/strings/substitute.h>
 
 void TakesStringView(std::string_view s) {
 
@@ -25,5 +27,31 @@ int main() {
   {
     auto ret = absl::StrCat("hello world", " hello abseil");
     std::cout << ret << std::endl;
+  }
+  {
+    std::cout << absl::Substitute("Hello substitute $0 ($1)", "abseil", 42) << std::endl;
+  }
+
+  {
+    std::vector<std::string> v = {"a", "b", "c"};
+    std::string s = absl::StrJoin(v, "-");
+    std::cout << "first join: " << s << std::endl;
+
+    std::vector<std::string_view> v2 = {"a", "b", "c"};
+    std::string s2 = absl::StrJoin(v, "-");
+
+    std::cout << "join string_view: " << s2 << std::endl;
+
+    std::vector<int> iv = {1, 2, 3};
+    std::string s3 = absl::StrJoin(iv, "-");
+    std::cout << "join int vector: " << s3 << std::endl;
+
+    const int a[] = {1, 2, 3};
+    std::string s4 = absl::StrJoin(a, "-");
+    std::cout << "join int array: " << s4 << std::endl;
+
+    std::map<std::string, int> m = {{"a", 1}, {"b", 2}, {"c", 3}};
+    std::string s5 = absl::StrJoin(m, ";", absl::PairFormatter("="));
+    std::cout << "join map: " << s5 << std::endl;
   }
 }
