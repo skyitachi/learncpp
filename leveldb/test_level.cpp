d6844de46b378d3d8080eb04a4e223e14c0a0903
Shiping Yao <shiping.yao@shopee.com>
2023-04-14 08:26:37 +0800

    add leveldb filter demo

--- leveldb/test_level.cpp
+++ leveldb/test_level.cpp
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <leveldb/db.h>
+#include <leveldb/filter_policy.h>
 #include <iostream>
 
 int main() {
@@ -11,8 +12,12 @@ int main() {
   options.compression = leveldb::CompressionType::kNoCompression;
   options.create_if_missing = true;
   options.reuse_logs = true;
-  
+
+  options.filter_policy = leveldb::NewBloomFilterPolicy(8);
+
   leveldb::Status status = leveldb::DB::Open(options, "test_level_db", &db);
+
+
   std::cout << "compression: " << options.compression << std::endl;
   if (!status.ok()) {
     std::cerr << "Unable to open/create test database './test_level_db'" << std::endl;
@@ -21,14 +26,15 @@ int main() {
   }
 
   leveldb::WriteOptions writeOptions;
-//  for (int i = 0; i < 1; i++) {
-//    db->Put(writeOptions, "k" + std::to_string(i), "v" + std::to_string(i));
-//  }
+  for (int i = 0; i < 1 << 20; i++) {
+    db->Put(writeOptions, "k" + std::to_string(i), "v" + std::to_string(i));
+  }
 //  leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
 //  for(it->SeekToFirst(); it->Valid(); it->Next()) {
 //    std::cout << it->key().ToString() << ":" << it->value().ToString() << std::endl;
 //  }
 //  delete it;
+
   delete db;
 }
 
