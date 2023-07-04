6541269fa76c3bc6b2a29ae80e5d065a7588a17e
Shiping Yao <shiping.yao@shopee.com>
2023-07-04 17:15:01 +0800

    arrow demo

--- arrow/arrow_demo.cpp
+++ arrow/arrow_demo.cpp
@@ -49,7 +49,44 @@ arrow::Result<std::shared_ptr<arrow::Table>> VectorToColumnarTable(
 
   	return table;
 }
+
+
+void array_demo() {
+  arrow::Int64Builder builder;
+  builder.Append(1);
+  builder.Append(2);
+  builder.Append(3);
+  builder.AppendNull();
+  builder.Append(5);
+  builder.Append(6);
+  builder.Append(7);
+  builder.Append(8);
+
+  auto maybe_array = builder.Finish();
+  if (!maybe_array.ok()) {
+    std::cout << "allocate failed" << std::endl;
+  }
+
+  std::shared_ptr<arrow::Array> array = *maybe_array;
+  auto int64_array = std::static_pointer_cast<arrow::Int64Array>(array);
+  const uint8_t* nullbitmap = int64_array->null_bitmap_data();
+
+  const int64_t* data = int64_array->raw_values();
+  int64_t index = 2;
+
+  for (int64_t i = 0; i < 8; i++) {
+    if (!int64_array->IsNull(i)) {
+      int64_t value = int64_array->Value(i);
+      std::cout << "index " << i << ", value: " << value << std::endl;
+    } else {
+      std::cout << "index " << i << " is null" << std::endl;
+    }
+  }
+  printf("nullmap value %d\n", *nullbitmap);
+}
+
 int main(int argc, char **argv) {
+  array_demo();
 	std::vector<data_row> rows = {
      {1, 1, {10.0}}, {2, 3, {11.0, 12.0, 13.0}}, {3, 2, {15.0, 25.0}}};
 
