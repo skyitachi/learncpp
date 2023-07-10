9569fc2d915d1ae4a10ac72659a76688538042bf
skyitachi <skbysp@gmail.com>
2023-07-10 21:22:17 +0800

    sync code

--- faiss_demo/main.cpp
+++ faiss_demo/main.cpp
@@ -4,6 +4,7 @@
 #include <faiss/index_io.h>
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFFlat.h>
+#include <faiss/impl/IDSelector.h>
 #include <faiss/impl/io.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -168,62 +169,91 @@ void merge_index_demo() {
   assert(index1.ntotal == index_read->ntotal);
 }
 
-int main() {
-//	faiss::IndexIVFFlat *index = dynamic_cast<faiss::IndexIVFFlat*>(
-//		faiss::read_index("data/saved_item.idx"));
-//  int d = 8;
-//  int nb = 100000;
-//  int nq = 10000;
-//
-//  float *xb = new float[d * nb];
-//  for(int i = 0; i < nb; i++) {
-//    for(int j = 0; j < d; j++) {
-//      xb[d * i + j] = drand48();
-//    }
-//  }
-//
-//  faiss::IndexFlatL2 quantizer(d);
-//  faiss::IndexIVFFlat index(&quantizer, d, 32);
-//  index.train(nb, xb);
-//  index.add(nb, xb);
-//
-//
-//  float *xq = new float[d * nq];
-//  for(int i = 0; i < nq; i++) {
-//    for(int j = 0; j < d; j++) {
-//      xq[d * i + j] = drand48();
-//    }
-//  }
-//
-//  int k = 4;
-//  idx_t *I = new idx_t[k * nq];
-//  float *D = new float[k * nq];
-//
-//  index.search(nq, xq, k, D, I);
-//
-//  for(int i = 0; i < 3; i++) {
-//    printf("Query %d:", i);
-//    for(int k = 0; k < d; k++) {
-//      printf(" %g", xq[i * d + k]);
-//    }
-//    printf("\n");
-//
-//    for(int j = 0; j < k; j++) {
-//      int idx = I[i * k + j];
-//      printf("  %d (distance=%g) origin vector: ", idx, D[i * k + j]);
-//      for (int k = 0; k < d; k++) {
-//        printf(" %g", xb[idx * d + k]);
-//      }
-//      printf("\n");
-//    }
-//  }
-//
-//  delete[] xb;
-//  delete[] xq;
-//  delete[] I;
-//  delete[] D;
+class CustomIDSelector: public faiss::IDSelector {
+public:
+  bool is_member(idx_t id) const override {
+    return id > 10;
+  }
 
-  merge_index_demo();
+};
+
+void search_with_params() {
+  int d = 3;
+  int nb = 100;
+  int nq = 1;
+
+  float *xb = new float[d * nb];
+  for(int i = 0; i < nb; i++) {
+    for(int j = 0; j < d; j++) {
+      xb[d * i + j] = i;
+    }
+  }
+  faiss::IndexFlatL2 quantizer(d);
+  faiss::IndexIVFFlat index(&quantizer, d, 32);
+  index.train(nb, xb);
+  index.add(nb, xb);
+
+  float *xq = new float[d * nq];
+
+  for(int i = 0; i < nq; i++) {
+    for(int j = 0; j < d; j++) {
+      xq[d * i + j] = drand48();
+    }
+  }
+
+  xq[0] = 0.0; xq[1] = 0.0; xq[2] = 0.0;
+
+
+  int k = 3;
+  idx_t *I = new idx_t[k * nq];
+  float *D = new float[k * nq];
+  index.search(1, xq, k, D, I);
+
+  for(int i = 0; i < 3; i++) {
+    printf("Query %d:", i);
+    for(int k = 0; k < d; k++) {
+      printf(" %g", xq[i * d + k]);
+    }
+    printf("\n");
+
+    for(int j = 0; j < k; j++) {
+      int idx = I[i * k + j];
+      printf("  %d (distance=%g) origin vector: ", idx, D[i * k + j]);
+      for (int k = 0; k < d; k++) {
+        printf(" %g", xb[idx * d + k]);
+      }
+      printf("\n");
+    }
+  }
+  auto* params = new faiss::SearchParameters;
+  params->sel = new CustomIDSelector;
+
+
+  index.search(1, xq, k, D, I, params);
+
+  for(int i = 0; i < 3; i++) {
+    printf("Query %d:", i);
+    for(int k = 0; k < d; k++) {
+      printf(" %g", xq[i * d + k]);
+    }
+    printf("\n");
+
+    for(int j = 0; j < k; j++) {
+      int idx = I[i * k + j];
+      printf("  %d (distance=%g) origin vector: ", idx, D[i * k + j]);
+      for (int k = 0; k < d; k++) {
+        printf(" %g", xb[idx * d + k]);
+      }
+      printf("\n");
+    }
+  }
+
+}
+
+int main() {
+  search_with_params();
+
+//  merge_index_demo();
 
 	return 0;
 }
