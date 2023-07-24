729e65a14cae45310216c627bba05e7ba44be08d
skyitachi <skbysp@gmail.com>
2023-07-24 23:23:03 +0800

    arrow nested data created

--- arrow/arrow_demo.cpp
+++ arrow/arrow_demo.cpp
@@ -2,6 +2,7 @@
 #include <arrow/result.h>
 #include <vector>
 #include <iostream>
+#include <random>
 
 using arrow::DoubleBuilder;
 using arrow::Int64Builder;
@@ -85,6 +86,152 @@ void array_demo() {
   printf("nullmap value %d\n", *nullbitmap);
 }
 
+// 一般使用内置api完成创建嵌套list
+arrow::Status arrow_nested_list_demo() {
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+
+  // 创建一个ListBuilder对象，用于构建一个整数列表
+  auto inner_list_builder =
+      std::make_shared<arrow::ListBuilder>(pool, std::make_shared<arrow::Int32Builder>(pool));
+
+  auto *inner_list_value_builder = static_cast<arrow::Int32Builder*>(inner_list_builder->value_builder());
+  inner_list_builder->Append();
+  inner_list_value_builder->Append(10);
+  inner_list_value_builder->Append(20);
+  inner_list_builder->Append(false);
+
+
+  auto list_builder =
+      std::make_shared<arrow::ListBuilder>(pool, inner_list_builder);
+
+  auto* list_value_build = static_cast<arrow::ListBuilder*>(list_builder->value_builder());
+
+  list_builder->Append();
+//  list_builder->AppendValues(inner_list_builder->Finish());
+
+  // 如何append
+
+//  list_value_build->AppendArraySlice(arr, 0, 2);
+
+  auto offset_builder = std::make_shared<arrow::Int32Builder>(pool);
+  int32_t rows  = 7;
+  offset_builder->Append(0);
+  offset_builder->Append(2);
+  offset_builder->Append(4);
+  offset_builder->Append( rows);
+
+  auto value_builder = std::make_shared<arrow::DoubleBuilder>();
+  for(int i = 0; i < rows; i++) {
+    value_builder->Append(i);
+  }
+  ARROW_ASSIGN_OR_RAISE(auto offsets, offset_builder->Finish());
+  ARROW_ASSIGN_OR_RAISE(auto values, value_builder->Finish());
+
+  ARROW_ASSIGN_OR_RAISE(auto array,
+                        arrow::ListArray::FromArrays(*offsets.get(), *values.get()));
+
+
+  auto outer_offsets_builder = std::make_shared<arrow::Int32Builder>(pool);
+  outer_offsets_builder->Append(0);
+  outer_offsets_builder->Append(2);
+  outer_offsets_builder->Append(3);
+
+  ARROW_ASSIGN_OR_RAISE(auto outer_offsets, outer_offsets_builder->Finish());
+
+  ARROW_ASSIGN_OR_RAISE(auto nested_array,
+                        arrow::ListArray::FromArrays(*outer_offsets.get(), *array.get()));
+
+
+
+  std::cout << array->ToString()  << std::endl;
+
+  std::cout << nested_array->ToString() << std::endl;
+
+
+
+  return arrow::Status::OK();
+
+
+}
+
+class RandomBatchGenerator {
+public:
+  std::shared_ptr<arrow::Schema> schema;
+
+  RandomBatchGenerator(std::shared_ptr<arrow::Schema> schema) : schema(schema){};
+
+  arrow::Result<std::shared_ptr<arrow::RecordBatch>> Generate(int32_t num_rows) {
+    num_rows_ = num_rows;
+    for (std::shared_ptr<arrow::Field> field : schema->fields()) {
+      ARROW_RETURN_NOT_OK(arrow::VisitTypeInline(*field->type(), this));
+    }
+
+    return arrow::RecordBatch::Make(schema, num_rows, arrays_);
+  }
+
+  // Default implementation
+  arrow::Status Visit(const arrow::DataType& type) {
+    return arrow::Status::NotImplemented("Generating data for", type.ToString());
+  }
+
+  arrow::Status Visit(const arrow::DoubleType&) {
+    auto builder = arrow::DoubleBuilder();
+    std::normal_distribution<> d{/*mean=*/5.0, /*stddev=*/2.0};
+    for (int32_t i = 0; i < num_rows_; ++i) {
+      ARROW_RETURN_NOT_OK(builder.Append(d(gen_)));
+    }
+    ARROW_ASSIGN_OR_RAISE(auto array, builder.Finish());
+    arrays_.push_back(array);
+    return arrow::Status::OK();
+  }
+
+  arrow::Status Visit(const arrow::ListType& type) {
+    // Generate offsets first, which determines number of values in sub-array
+    std::poisson_distribution<> d{/*mean=*/4};
+    auto builder = arrow::Int32Builder();
+    ARROW_RETURN_NOT_OK(builder.Append(0));
+    int32_t last_val = 0;
+    for (int32_t i = 0; i < num_rows_; ++i) {
+      last_val += d(gen_);
+      ARROW_RETURN_NOT_OK(builder.Append(last_val));
+    }
+    ARROW_ASSIGN_OR_RAISE(auto offsets, builder.Finish());
+
+    // Since children of list has a new length, will use a new generator
+    RandomBatchGenerator value_gen(arrow::schema({arrow::field("x", type.value_type())}));
+    // Last index from the offsets array becomes the length of the sub-array
+    ARROW_ASSIGN_OR_RAISE(auto inner_batch, value_gen.Generate(last_val));
+    std::shared_ptr<arrow::Array> values = inner_batch->column(0);
+
+    ARROW_ASSIGN_OR_RAISE(auto array,
+                          arrow::ListArray::FromArrays(*offsets.get(), *values.get()));
+    arrays_.push_back(array);
+
+    return arrow::Status::OK();
+  }
+
+protected:
+  std::random_device rd_{};
+  std::mt19937 gen_{rd_()};
+  std::vector<std::shared_ptr<arrow::Array>> arrays_;
+  int32_t num_rows_;
+};  // RandomBatchGenerator
+
+arrow::Status create_arrays_with_schema_demo() {
+  std::shared_ptr<arrow::Schema> schema =
+      arrow::schema({arrow::field("x", arrow::float64()),
+                     arrow::field("y", arrow::list(arrow::float64()))});
+
+  RandomBatchGenerator generator(schema);
+  ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::RecordBatch> batch, generator.Generate(5));
+
+  std::cout << "Created batch: \n" << batch->ToString();
+// Consider using ValidateFull to check correctness
+  ARROW_RETURN_NOT_OK(batch->ValidateFull());
+
+  return arrow::Status::OK();
+}
+
 arrow::Status arrow_list_demo() {
 //  std::shared_ptr<arrow::DataType> type;
 //  type = arrow::int16();
@@ -139,6 +286,8 @@ arrow::Status arrow_list_demo() {
 }
 
 int main(int argc, char **argv) {
+  arrow_nested_list_demo();
+  create_arrays_with_schema_demo();
   arrow_list_demo();
   array_demo();
 	std::vector<data_row> rows = {
