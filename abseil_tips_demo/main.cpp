2b87b51a5ef117a15f0a3cd7c35358b901a85485
skyitachi <skbysp@gmail.com>
2023-10-26 08:32:55 +0800

    abseil tips　demo

--- abseil_tips_demo/main.cpp
+++ abseil_tips_demo/main.cpp
@@ -7,7 +7,8 @@
 #include <absl/strings/str_join.h>
 #include <absl/strings/str_replace.h>
 #include <absl/strings/substitute.h>
-
+#include <list>
+#include <optional>
 void TakesStringView(std::string_view s) {
 
 }
@@ -18,6 +19,64 @@ void AlreadyHasString(const std::string& s) {
 
 void AlreadyHasCharStar(const char* s) {
   TakesStringView(s);
+}
+
+
+// name lookup first and then ADL
+//namespace b {
+//  void func(const std::string&);  // b::func
+//  namespace internal {
+//    void func(int);  // b::internal::func
+//    namespace deep {
+//      void test() {
+//        std::string s("hello");
+//        // no argument dependent lookup, just name lookup
+//        func(s);  // error: finds only b::internal::func(int).
+//      }
+//    }  // b::internal::deep
+//  }  // b::internal
+//}  // b
+
+namespace aspace {
+  struct A {};
+  void func(const A&);  // found by ADL name lookup on 'a'.
+}  // namespace aspace
+
+namespace bspace {
+  void func(int);  // found by lexical scope name lookup
+  void test() {
+    aspace::A a;
+    func(a);  // aspace::func(const aspace::A&) due to ADL
+  }
+}  // namespace bspace
+
+class Foo{
+public:
+  explicit Foo(int i) {}
+  void PrintDebugString() {}
+};
+
+std::unique_ptr<Foo> NewFoo() {
+  return std::unique_ptr<Foo>(new Foo(1));
+}
+
+void AcceptFoo(std::unique_ptr<Foo> f) { f->PrintDebugString(); }
+
+void Simple() {
+  AcceptFoo(NewFoo());
+}
+
+void DoesNotBuild() {
+  std::unique_ptr<Foo> g = NewFoo();
+//  AcceptFoo(g); // DOES NOT COMPILE!
+  AcceptFoo(std::move(g)); // DOES NOT COMPILE!
+}
+
+void SmarterThanTheCompilerButNot() {
+  Foo* j = new Foo(2);
+  // Compiles, BUT VIOLATES THE RULE and will double-delete at runtime.
+  std::unique_ptr<Foo> k(j);
+  std::unique_ptr<Foo> l(j);
 }
 
 int main() {
@@ -53,5 +112,26 @@ int main() {
     std::map<std::string, int> m = {{"a", 1}, {"b", 2}, {"c", 3}};
     std::string s5 = absl::StrJoin(m, ";", absl::PairFormatter("="));
     std::cout << "join map: " << s5 << std::endl;
+  }
+
+  {
+    std::vector<std::string_view> v = absl::StrSplit("a,b,c", ',');
+    for(auto& item: v) {
+      std::cout << " " << item;
+    }
+    std::cout << std::endl;
+
+    std::vector<absl::string_view> v1 = absl::StrSplit("a,b,c", ',');
+    for(auto& item: v1) {
+      std::cout << " " << item;
+    }
+    std::cout << std::endl;
+
+    std::set<std::string> s = absl::StrSplit("a,b,c", ',');
+    std::cout << "split to sets: " << s.size() << std::endl;
+
+    std::list<std::string> s1= absl::StrSplit("a,b,c", ",");
+    std::cout << "split to list: " << s1.size() << std::endl;
+
   }
 }
